7623b55ac17ebb007921770f9163cac272113a37
Eugene Kovalev <seanchas@gmail.com>
2016-05-12 17:56:08 +0300

    Time processing

--- utils/parse_time.js
+++ utils/parse_time.js
@@ -1,8 +1,12 @@
 import moment from 'moment'
 
-const TimeFormats = ['H', 'HH', 'H:mm', 'HH:mm', 'Hmm', 'HHmm', 'H.mm', 'HH.mm', 'ha', 'h a']
+const TimeFormats = ['H', 'HH', 'Hmm', 'HHmm', 'ha', 'hmma']
 
 export default function(value) {
+  value = value.toLowerCase()
+  let ampm = value.match(/am|pm$/)
+  ampm = ampm ? ampm[0] : ''
+  value = value.replace(/[^\d]/g, '').trim() + ampm
   let result = TimeFormats.find(format => moment(value, format, true).isValid())
   return result
     ? moment(value, result).format('HH:mm')
